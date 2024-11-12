c4e719b8e1e9f21775720ffa0176effa1870872b
Hugo Silva <Hugo0312@outlook.com>
2024-11-12 01:58:47 -0300

    criação inicio classe cliente

---
+++ XulambsFoods_Atualizado/Cliente.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XulambsFoods_Atualizado
+{
+    class Cliente
+    {
+        private string _nome;
+        private string _cpf;
+        private Queue<Pedido> _todosPedidos;
+
+
+        public Cliente(string nome, string cpf)
+        {
+            this._nome = nome;
+            this._cpf = cpf;
+            _todosPedidos = new Queue<Pedido>();
+        }
+
+        public string GetNome()
+        {
+            return _nome;
+        }
+
+        public string GetCPF()
+        {
+            return _cpf;
+        }
+
+        
+    }
+}
